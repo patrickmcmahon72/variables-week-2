a0aea52ccd73b86488385effe447609b0f2b7f67
patrickmcmahon72 <35769415+patrickmcmahon72@users.noreply.github.com>
2018-01-31 13:38:10 +0000

    Add files via upload

---
+++ js/string-variable.js
@@ -0,0 +1,17 @@
+// create variables to hold the name and note text.
+var username;
+var message;
+
+//assign values to these variables.
+username = 'Patrick';
+message = "Hey " + username + ' See our upcoming range';
+
+//Get the element with an id of name.
+var elName = document.getElementById('name');
+//Replace the content of this element.
+elName.textContent = username;
+
+//Get the element with an id of note.
+var elNote = document.getElementById('note');
+//Replace the content of this element.
+elNote.textContent = message;
